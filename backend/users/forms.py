05fc13b14c8854792a9ac032a9c19ec1e366bb11
ilyasurkov94 <ilyasurkov1994@mail.ru>
2022-03-20 20:31:00 +0300

    Add FormSet

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -3,5 +3,6 @@ from django import forms
 
 class IngredientAmountFormset(forms.models.BaseInlineFormSet):
     def clean(self):
+        raise forms.ValidationError(f'{self.cleaned_data}')
         if not self.cleaned_data:
             raise forms.ValidationError('Заполните данные ингредиентов')
