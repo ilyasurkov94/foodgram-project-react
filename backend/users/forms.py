c93926602ef9ae68ac7a85e63ea3c8a7761c62f5
ilyasurkov94 <ilyasurkov1994@mail.ru>
2022-03-20 22:49:20 +0300

    Update FormSet

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -4,6 +4,7 @@ from django.forms import ValidationError
 
 class IngredientAmountFormset(BaseInlineFormSet):
     def clean(self):
+        raise ValidationError(f'{self.exclude}')
         # Если в админке удалить форму для ингредиентов
         if self.forms == []:
             raise ValidationError('Добавьте минимум один ингредиент')
