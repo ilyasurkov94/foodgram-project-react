e62bdb11dcdd24aa93530f26be04b2ee38fcd879
ilyasurkov94 <ilyasurkov1994@mail.ru>
2022-03-20 22:43:06 +0300

    Add FormSet

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -4,14 +4,11 @@ from django.forms import ValidationError
 
 class IngredientAmountFormset(BaseInlineFormSet):
     def clean(self):
+        # Если в админке удалить форму для ингредиентов
         if self.forms == []:
             raise ValidationError('Добавьте минимум один ингредиент')
-        if self.cleaned_data is None:
-            raise ValidationError('Заполните данные ингредиентов')
+        # Если форма есть, но данные пустые
+        # if self.cleaned_data is None:
+        #     raise ValidationError('Заполните данные ингредиентов')
         if self.cleaned_data == [{}]:
             raise ValidationError('Заполните данные ингредиентов')
-        for data in self.cleaned_data:
-            if data.get('ingredient') is None:
-                raise ValidationError('Добавьте ингредиент')
-            elif data.get('amount') is None:
-                raise ValidationError('Заполните количество ингредиента')
