f853cc0a3af35838acac75c3e4b067b17b2b557a
ilyasurkov94 <ilyasurkov1994@mail.ru>
2022-03-20 20:17:04 +0300

    FormSet check

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -3,5 +3,6 @@ from django import forms
 
 class IngredientAmountFormset(forms.models.BaseInlineFormSet):
     def clean(self):
-        if not self.cleaned.data:
+        raise forms.ValidationError(f'{self.cleaned_data}')
+        if not self.cleaned_data:
             raise forms.ValidationError('Заполните данные')
