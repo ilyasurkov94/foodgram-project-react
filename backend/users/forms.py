b229f769613f1e28bcc47b99bd0601d53ad99530
ilyasurkov94 <ilyasurkov1994@mail.ru>
2022-03-20 20:57:53 +0300

    add setform

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,14 +1,16 @@
-from django import forms
+from django.forms.models import BaseInlineFormSet
+from django.forms import ValidationError
 
 
-class IngredientAmountFormset(forms.models.BaseInlineFormSet):
+class IngredientAmountFormset(BaseInlineFormSet):
     def clean(self):
+        raise ValidationError(f'{self.forms}')
         if self.cleaned_data is None:
-            raise forms.ValidationError('Заполните данные ингредиентов')
+            raise ValidationError('Заполните данные ингредиентов')
         if self.cleaned_data == [{}]:
-            raise forms.ValidationError('Заполните данные ингредиентов')
+            raise ValidationError('Заполните данные ингредиентов')
         for data in self.cleaned_data:
             if data.get('ingredient') is None:
-                raise forms.ValidationError('Добавьте ингредиент')
+                raise ValidationError('Добавьте ингредиент')
             elif data.get('amount') is None:
-                raise forms.ValidationError('Заполните количество ингредиента')
+                raise ValidationError('Заполните количество ингредиента')
